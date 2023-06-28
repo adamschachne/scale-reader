139f8d1d93b95d066195a022b054984891f76323
Adam Schachne <adamschachne@live.com>
2023-06-27 19:02:17 -0700

    webhid displaying weight

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,89 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import styles from "./App.module.css";
+
+const hasHid = "hid" in navigator;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [hidDevice, setHidDevice] = useState<HIDDevice | null>(null);
+  const [grams, setGrams] = useState<number>(0);
+
+  const pounds = Math.round((grams / 453.59237) * 100) / 100;
+  const ounces = Math.round(pounds * 16);
+
+  const openDevice = (device: HIDDevice) => {
+    device
+      .open()
+      .then(() => {
+        console.log("Opened device: " + device.productName);
+
+        device.addEventListener("inputreport", (event) => {
+          const { data } = event;
+          // Byte 0 == Report ID?
+          // Byte 1 == Scale Status (1 == Fault, 2 == Stable @ 0, 3 == In Motion, 4 == Stable, 5 == Under 0, 6 == Over Weight, 7 == Requires Calibration, 8 == Requires Re-Zeroing)
+          // Byte 2 == Weight Unit
+          // Byte 3 == Data Scaling (decimal placement)
+          // Byte 4 == Weight LSB
+          // Byte 5 == Weight MSB
+
+          const bytes = new Uint8Array(data.buffer);
+          const weightUnit = bytes[1];
+
+          switch (weightUnit) {
+            case 2: // grams
+              setGrams(Math.round((bytes[3] + bytes[4] * 256) * 100) / 100);
+              break;
+            case 11: // ounces
+              setGrams(Math.round(((bytes[3] + bytes[4] * 256) / 10) * 28.3495 * 100) / 100);
+              break;
+            case 12: // pounds
+            default:
+              console.log("idk");
+          }
+        });
+      })
+      .catch((error) => {
+        console.log("Failed to open device: " + error);
+      });
+  };
+
+  const requestDevice = () => {
+    navigator.hid
+      .requestDevice({ filters: [] })
+      .then((devices) => {
+        if (devices.length == 0) {
+          console.log("No device was selected.");
+        } else {
+          console.log("Device selected: " + devices[0].productName);
+          setHidDevice(devices[0]);
+          openDevice(devices[0]);
+        }
+      })
+      .catch((err) => {
+        console.log("An error occurred: " + err);
+      });
+  };
+
+  if (!hasHid) {
+    return <h1>WebHID not supported</h1>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className={styles.app}>
+      {hidDevice == null ? (
+        <button className={styles["select-device-button"]} onClick={requestDevice}>
+          Select device
+        </button>
+      ) : (
+        <div>
+          <h2>{hidDevice.productName}</h2>
+          <div style={{ fontSize: "2em" }}>
+            <div>{grams} g</div>
+            <div>{pounds} lbs</div>
+            <div>{ounces} oz</div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
